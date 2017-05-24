964e9ff61ee556e5e040ec154395a53a89976b2c
swgoodman33 <swgoodman33@users.noreply.github.com>
2017-05-24 09:21:10 -0500

    Latest - 5/23

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  # belongs_to :user, :class_name => "User", :foreign_key => "user_id"
-  # belongs_to :photo, :class_name => "Photo", :foreign_key => "photo_id"
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+  belongs_to :photo, :class_name => "Photo", :foreign_key => "photo_id"
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
-  # belongs_to :user, :class name => "User", :foreign_key => "user_id"
-  # has_many :comments
-  # has_many :likes
-  # has_many :fans, :through => :likes, :source => :user
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+  has_many :comments
+  has_many :likes
+  has_many :fans, :through => :likes, :source => :user
 end
